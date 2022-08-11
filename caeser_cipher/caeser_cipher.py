a437cfa113879bb4c366e03e2ac496afc1855890
Vinny <vinnyshipley@gmail.com>
2022-08-10 23:01:29 -0700

    8/10 finish

--- caeser_cipher/caeser_cipher.py
+++ caeser_cipher/caeser_cipher.py
@@ -1,10 +1,29 @@
 
 
-def encrypt(string, shift):
-  pass
+def encrypt(string,shift):
+  encrypted = list(str(string))
+  return_list = []
+  for char in encrypted:
+    num = ord(char)
+    print(f'this is num{num}')
+    big_val = num + shift
+    print(f'this is big val{big_val}')
+    new_key = big_val % 26
+    print(f'new key {new_key}')
+    new_char_val = (new_key + 97)
+    new_char = chr(new_char_val)
+    return_list.append(new_char)
+  string = ''
+  new_string = string.join(return_list)
+  print(new_string)
+  print(ord('b'))
+  print(ord('u'))
+  return new_string
+
 
 def decrypt(string, key):
   pass
 
 def crack(string):
   pass
+
